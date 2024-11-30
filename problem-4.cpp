a8548fd930ede3bf3c928a480d2b6f7b67328266
ehsanul1100 <mdehsanul1100@gmail.com>
2024-11-30 19:17:40 +0600

    Add new file to repository

---
+++ problem-4.cpp
@@ -0,0 +1,19 @@
+// maximum and minimum in devide and conquier method 
+
+#include <bits/stdc++.h>
+using namespace std;
+int findMaxMin(vector<int>,int first,int last){
+    
+}
+int main(){
+    vector<int> vec;
+    int n, element;
+    cout << "Enter the size of the arry : ";
+    cin >> n;
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> element;
+        vec.push_back(element);
+    }
+    return 0;
+}
